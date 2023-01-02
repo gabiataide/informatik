0d166330b6b671ae45ccf8eb8c6d0ec1058921ae
Gabriela de Ataide Magalhaes <gabrielaam@Gabrielas-MacBook-Air.local>
2023-01-02 17:41:58 +0100

    my commit message

---
+++ informatik.py
@@ -0,0 +1,28 @@
+import random
+
+chores = ["Clean WC", "Laundry (Bedding)", "Laundry (Clothes)", "Vacuuming", "Groceries", "Fold Clothes",
+          "Meal Prep", "Clean Bathroom", "Change Bedsheets", "Reorganize Cabinets", "Hang Clothes to dry",
+          "Clean Kitchen", "Mop Floors", "Dust the Living Room"]
+random.shuffle(chores)
+
+
+def gabiclean():
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    names = []
+    add_name = input("\nEnter your name: ")
+    names.append(add_name)
+    print("\n*** Weekly household chores for", add_name, "*** \n")
+
+    for day in week:
+        for name in names:
+            chore = chores.pop()
+            if name == "Jonas":
+                assign = day + " " + " - " + chore + " and Cook Dinner"
+                print(assign)
+
+            else:
+                assign = day + " " + " - " + chore
+                print(assign)
+
+
+gabiclean() 
